f92595048cb1ebb16bfa59cefd7f25d7b4815ccf
Elbert <elbertnatan@hotmail.com>
2023-08-23 09:15:46 -0300

    formatando

--- app.ts
+++ app.ts
@@ -1,18 +1,18 @@
-const person :{
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string];
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
 } = {
-    name: 'Elbert',
-    age: 21,
-    hobbies: ['Programming', 'Gaming'],
-    role: [1, 'owner']
+  name: "Elbert",
+  age: 21,
+  hobbies: ["Programming", "Gaming"],
+  role: [1, "owner"],
 };
 
 console.log(person.name);
 console.log(person.age);
 
-for(const hobby of person.hobbies){
-    console.log(hobby);
+for (const hobby of person.hobbies) {
+  console.log(hobby);
 }
