ee672c5e8fd0d299c3b384dfda1308373f57ae62
Elbert <elbertnatan@hotmail.com>
2023-08-23 09:08:37 -0300

    testando arrays

--- app.ts
+++ app.ts
@@ -1,7 +1,12 @@
 const person = {
     name: 'Elbert',
-    age: 21
+    age: 21,
+    hobbies: ['Programming', 'Gaming']
 };
 
 console.log(person.name);
 console.log(person.age);
+
+for(const hobby of person.hobbies){
+    console.log(hobby);
+}
