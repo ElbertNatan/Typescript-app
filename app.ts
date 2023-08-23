8e674fa17a04e7b215a08a6d4e7f50872e2cf5d8
Elbert <elbertnatan@hotmail.com>
2023-08-23 12:00:06 -0300

    testando função em parametro

--- app.ts
+++ app.ts
@@ -5,8 +5,16 @@ function add(n1: number, n2: number): number {
 function printResult(num: number): void {
   console.log("Result: " + num);
 }
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
 
 let combinedValues: (a: number, b: number) => number;
 combinedValues = add;
 
 console.log(combinedValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
