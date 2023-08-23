eaa8e921cf5c7813fbcdde32df92262d88588d38
Elbert <elbertnatan@hotmail.com>
2023-08-23 08:58:57 -0300

    testando objetos

--- app.ts
+++ app.ts
@@ -1,15 +1,7 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-    const result = n1 + n2;
-    if (showResult) {
-      console.log(phrase + result);
-    } else {
-      return result;
-    }
-  }
-  
-  const number1 = 5; // 5.0
-  const number2 = 2.8;
-  const printResult = true;
-  const resultPhrase = 'Result is: ';
-  
-  add(number1, number2, printResult, resultPhrase);
+const person = {
+    name: 'Elbert',
+    age: 21
+};
+
+console.log(person.name);
+console.log(person.age);
