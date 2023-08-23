de13bf408a0ac6fbfaabcd0cfb006f7106b182ae
Elbert <elbertnatan@hotmail.com>
2023-08-23 12:04:51 -0300

    testando unknown type

--- app.ts
+++ app.ts
@@ -1,20 +1,6 @@
-function add(n1: number, n2: number): number {
-  return n1 + n2;
-}
+let userInput: unknown;
+let userName: string;
 
-function printResult(num: number): void {
-  console.log("Result: " + num);
-}
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-let combinedValues: (a: number, b: number) => number;
-combinedValues = add;
-
-console.log(combinedValues(8, 8));
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-});
+userInput = 5;
+userInput = "Max";
+if (typeof userInput === "string") userName = userInput;
